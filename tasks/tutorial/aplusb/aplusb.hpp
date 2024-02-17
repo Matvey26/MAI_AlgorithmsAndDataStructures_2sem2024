e09236d4d355af643785ead27bf23cc9da1c028b
- - <>
2024-02-17 15:38:10 +0300

    second test

--- tasks/tutorial/aplusb/aplusb.hpp
+++ tasks/tutorial/aplusb/aplusb.hpp
@@ -2,14 +2,16 @@
 
 int Sum(int a, int b) {
     for (int i = 0; i < 2; ++i) {
-        if (a == INT32_MAX and b > 0)
+        if (a == INT32_MAX and b > 0) {
             return INT32_MAX;
+        }
         std::swap(a, b);
     }
 
     for (int i = 0; i < 2; ++i) {
-        if (a == INT32_MIN and b < 0)
+        if (a == INT32_MIN and b < 0) {
             return INT32_MIN;
+        }
         std::swap(a, b);
     }
 
