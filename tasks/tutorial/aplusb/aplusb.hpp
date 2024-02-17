acc76cb95c8ef7c902ff1a1362214b09fcaf0bf2
- - <>
2024-02-17 15:31:44 +0300

    first test

--- tasks/tutorial/aplusb/aplusb.hpp
+++ tasks/tutorial/aplusb/aplusb.hpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
-int Sum(int /*a*/, int /*b*/) {
-    std::abort(); // Not implemented.
+int Sum(int a, int b) {
+    for (int i = 0; i < 2; ++i) {
+        if (a == INT32_MAX and b > 0)
+            return INT32_MAX;
+        std::swap(a, b);
+    }
+
+    for (int i = 0; i < 2; ++i) {
+        if (a == INT32_MIN and b < 0)
+            return INT32_MIN;
+        std::swap(a, b);
+    }
+
+    return a + b;
 }
