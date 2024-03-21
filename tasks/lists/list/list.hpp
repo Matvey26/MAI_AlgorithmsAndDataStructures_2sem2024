f081dad0b045bf5ada2070a2456418bbd4cb9746
- - <>
2024-03-21 23:28:35 +0300

    Implemented Node, Almost implemented ListIterator, Update List

--- tasks/lists/list/list.hpp
+++ tasks/lists/list/list.hpp
@@ -1,172 +1,211 @@
 #pragma once
 
-#include <cstdlib>
 #include <cstddef>
-#include <iterator>
+#include <cstdlib>
 #include <functional>
+#include <iterator>
 #include <utility>
 
-#include <fmt/core.h>
+// #include <fmt/core.h>
 
 template <typename T>
-class List{
+class List {
 private:
-  class Node{
-    friend class ListIterator;
-    friend class List;
-    /*???*/
+    class Node {
+        friend class ListIterator;
+        friend class List;
 
     private:
-      /*???*/
-
-  };
+        T data;
+        Node* next;
+        Node* prev;
+    };
 
 public:
-  class ListIterator{
+    class ListIterator {
+        friend class List;
+
     public:
-      using value_type = T;
-      using reference_type = value_type&;
-      using pointer_type = value_type*;
-      using difference_type = std::ptrdiff_t;
-      using iterator_category = std::bidirectional_iterator_tag;
+        using value_type = T;
+        using reference_type = value_type&;
+        using pointer_type = value_type*;
+        using difference_type = std::ptrdiff_t;
+        using iterator_category = std::bidirectional_iterator_tag;
 
-      inline bool operator==(const ListIterator&) const {
-          std::abort(); // Not implemented
-      };
+        ListIterator& operator=(ListIterator&) = default;
 
-      inline bool operator!=(const ListIterator&) const {
-          std::abort(); // Not implemented
-      };
+        inline bool operator==(const ListIterator& other) const {
+            return this->current == other.current;
+        };
 
-      inline reference_type operator*() const {
-          std::abort(); // Not implemented
-      };
+        inline bool operator!=(const ListIterator& other) const {
+            return this->current != other.current;
+        };
 
-      ListIterator& operator++() {
-          std::abort(); // Not implemented
-      };
+        inline reference_type operator*() const {
+            return current->data;
+        };
 
-      ListIterator operator++(int) {
-          std::abort(); // Not implemented
-      };
+        ListIterator& operator++() {
+            current = current->next;
+            return *this;
+        };
 
-      ListIterator& operator--() {
-          std::abort(); // Not implemented
-      };
+        ListIterator operator++(int) {
+            ListIterator copy = *this;
+            current = current->next;
+            return copy;
+        };
 
-      ListIterator operator--(int) {
-          std::abort(); // Not implemented
-      };
+        ListIterator& operator--() {
+            current = current->prev;
+            return *this;
+        };
 
-      /*The overload of operator -> must either return a raw pointer, 
-      or return an object (by reference or by value) for which 
-      operator -> is in turn overloaded.*/
-      inline pointer_type operator->() const {
-          std::abort(); // Not implemented
-      };
+        ListIterator operator--(int) {
+            ListIterator copy = *this;
+            current = current->prev;
+            return copy;
+        };
 
-  private:
-      ListIterator(const Node*) {
-          // Not implemented
-      }
-  private:
-      Node* current;
-  };
+        inline pointer_type operator->() const {
+            return &current->data;
+        };
+
+    private:
+        ListIterator(Node* other_node) {
+            current = other_node;
+        }
+        ListIterator(const Node* other_node) {
+            current = other_node;
+        }
+
+    private:
+        Node* current;
+    };
 
 public:
-  List() {
-    // Not implemented
-  }
+    List() {
+        head = new Node();
+        head->next = head->prev = head;
+        size = 0;
+    }
 
-  explicit List(size_t /*sz*/) {
-    // Not implemented
-  }
+    explicit List(size_t sz) : size(sz) {
+        head = new Node();
+        head.next = head.prev = head;
+    }
 
-  List(const std::initializer_list<T>& /*values*/) {
-    // Not implemented
-  }
+    List(const std::initializer_list<T>& values) : List() {
+        for (T value : values) {
+            PushBack(value);
+            ++size;
+        }
+    }
 
-  List(const List& /*other*/) {
-    // Not implemented
-  }
+    List(const List& other) : List() {
+        for (List<T>::ListIterator current = other.Begin(); current != other.End(); ++current) {
+            PushBack(*current);
+        }
+    }
 
-  List& operator=(const List& /*other*/) {
-    std::abort(); // Not implemented
-  }
+    List& operator=(const List& other) {
+        // if (this == &other) {
+        //   return *this;
+        // }
+        // List<T>::ListIterator this_iterator = Begin();
+        // List<T>::ListIterator other_iterator = Begin();
+        // while (other_iterator != other.End()) {
+        //   if (this_iterator == End()) {
+        //     PushBack(*other_iterator);
+        //     ++other_iterator;
+        //   }
+        //   *this_iterator = *other_iterator;
+        //   ++this_iterator;
+        //   ++other_iterator;
+        // }
+        // return *this;
+    }
 
-  ListIterator Begin() const noexcept {
-    std::abort(); // Not implemented
-  }
+    ListIterator Begin() const noexcept {
+        ListIterator begin_iterator(head->next);
+        return begin_iterator;
+    }
 
-  ListIterator End() const noexcept {
-    std::abort(); // Not implemented
-  }
+    ListIterator End() const noexcept {
+        ListIterator end_iterator(head);
+        return end_iterator;
+    }
 
-  inline T& Front() const {
-    std::abort(); // Not implemented
-  }
+    inline T& Front() const {
+        return head->next->data;
+    }
 
-  inline T& Back() const {
-    std::abort(); // Not implemented
-  }
+    inline T& Back() const {
+        std::abort();  // Not implemented
+    }
 
-  inline bool IsEmpty() const noexcept {
-    std::abort(); // Not implemented
-  }
+    inline bool IsEmpty() const noexcept {
+        std::abort();  // Not implemented
+    }
 
-  inline size_t Size() const noexcept {
-    std::abort(); // Not implemented
-  }
+    inline size_t Size() const noexcept {
+        std::abort();  // Not implemented
+    }
 
-  void Swap(List& /*a*/) {
-    // Not implemented
-  }
+    void Swap(List& /*a*/) {
+        // Not implemented
+    }
 
-  ListIterator Find(const T& /*value*/) const {
-    std::abort(); // Not implemented
-  }
+    ListIterator Find(const T& /*value*/) const {
+        std::abort();  // Not implemented
+    }
 
-  void Erase(ListIterator /*pos*/) {
-    // Not implemented
-  }
+    void Erase(ListIterator /*pos*/) {
+        // Not implemented
+    }
 
-  void Insert(ListIterator /*pos*/, const T& /*value*/) {
-    // Not implemented
-  }
+    void Insert(ListIterator /*pos*/, const T& /*value*/) {
+        // Not implemented
+    }
 
-  void Clear() noexcept {
-    // Not implemented
-  }
+    void Clear() noexcept {
+        // Not implemented
+    }
 
-  void PushBack(const T& /*value*/) {
-    // Not implemented
-  }
+    void PushBack(const T& value) {
+        Node* node_to_add = new Node();
+        node_to_add->data = value;
+        node_to_add->next = head;
+        head->prev->next = node_to_add;
+        head->prev = node_to_add;
+    }
 
-  void PushFront(const T& /*value*/) {
-    // Not implemented
-  }
+    void PushFront(const T& /*value*/) {
+        // Not implemented
+    }
 
-  void PopBack() {
-    // Not implemented
-  }
+    void PopBack() {
+        // Not implemented
+    }
 
-  void PopFront() {
-    // Not implemented
-  }
+    void PopFront() {
+        // Not implemented
+    }
 
-  ~List() {
-    // Not implemented
-  }
+    ~List() {
+        // Not implemented
+    }
 
 private:
-  /*???*/
+    Node* head;
+    size_t size;
 };
 
-
 namespace std {
-  // Global swap overloading
-  template <typename T>
-  void swap(List<T>& a, List<T>& b) {
+// Global swap overloading
+template <typename T>
+void swap(List<T>& a, List<T>& b) {
     a.Swap(b);
-  }
 }
+}  // namespace std
