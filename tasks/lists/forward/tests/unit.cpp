07e7c52b22a265a438422c5e381faca9b7a5ae4b
- - <>
2024-03-25 16:07:34 +0300

    Update from https://github.com/Maxsmile123/Algorithms-And-DataStructure-Course/tree/master

--- tasks/lists/forward/tests/unit.cpp
+++ tasks/lists/forward/tests/unit.cpp
@@ -7,6 +7,8 @@
 
 #include "../forward_list.hpp"
 
+#include <iostream>
+
 class ListTest: public testing::Test {
   protected:
     void SetUp() override {
@@ -24,11 +26,13 @@ class ListTest: public testing::Test {
 };
 
 
+// OK
 TEST(EmptyListTest, DefaultConstructor) {
   ForwardList<int> list;
   ASSERT_TRUE(list.IsEmpty()) << "Default list isn't empty!";
 }
 
+// OK
 TEST(EmptyListTest, PushFrontSimple) {
   ForwardList<int> list;
   list.PushFront(2);
@@ -36,6 +40,7 @@ TEST(EmptyListTest, PushFrontSimple) {
   ASSERT_EQ(list.Front(), 2);
 }
 
+// OK
 TEST(EmptyListTest, PopFrontSimple) {
   ForwardList<int> list;
   list.PushFront(1);
@@ -46,6 +51,7 @@ TEST(EmptyListTest, PopFrontSimple) {
   ASSERT_EQ(list.Front(), 1);
 }
 
+// OK
 TEST(EmptyListTest, PopFrontEmptyList) {
   ForwardList<int> list;
   EXPECT_THROW({
@@ -53,6 +59,7 @@ TEST(EmptyListTest, PopFrontEmptyList) {
   }, std::runtime_error);
 }
 
+// OK
 TEST(EmptyListTest, ConstructorSizeDefaultValues) {
   ForwardList<std::string> list(5);
   ASSERT_EQ(list.Size(), 5);
@@ -62,6 +69,7 @@ TEST(EmptyListTest, ConstructorSizeDefaultValues) {
   }
 }
 
+// OK
 TEST(EmptyListTest, ConstructorWithInitList) {
   ForwardList<int> list{1, 2, 3, 4, 5, 6, 7, 8};
   ASSERT_EQ(list.Size(), 8);
@@ -72,11 +80,12 @@ TEST(EmptyListTest, ConstructorWithInitList) {
   }
 }
 
+// NOT OK
 TEST(EmptyListTest, Swap) {
-  ForwardList<int> list;
+  ForwardList<int> list;  // {5}
   list.PushFront(5);
 
-  ForwardList<int> lst;
+  ForwardList<int> lst;  // {14, 15}
   lst.PushFront(15);
   lst.PushFront(14);
 
@@ -89,27 +98,49 @@ TEST(EmptyListTest, Swap) {
   ASSERT_EQ(list.Size(), old_dict_size);
 
   ASSERT_EQ(lst.Front(), 5);
-  ASSERT_EQ(list.Front(), 15);
-  list.PopFront();
+
+  // ASSERT_EQ(list.Front(), 15);  // После свопа список list равен {14, 15} 
   ASSERT_EQ(list.Front(), 14);
+
+  list.PopFront();
+
+  // ASSERT_EQ(list.Front(), 14);  // После PopFront список list равен {15}
+  ASSERT_EQ(list.Front(), 15);
 }
 
+// NOT OK
 TEST_F(ListTest, CopyConstructor) {
-  ForwardList<int> lst = list;
+  ForwardList<int> lst = list;  // lst = {1, 2, 3, 4, 5, 6, 7}
   ASSERT_NE(&list, &lst) << "Copy constructor must do copy!\n";
-  ASSERT_EQ(list.Size(), lst.Size());
-  while (!lst.IsEmpty()) {
-    ASSERT_EQ(list.Front(), lst.Front());
-    list.PopFront();
-    ASSERT_NE(list.Front(), lst.Front());
+  ASSERT_EQ(list.Size(), lst.Size());  // 7 == 7
+
+  /*
+
+  while (!lst.IsEmpty()) {  // На последней итерации list = {7} и lst = {7}
+    ASSERT_EQ(list.Front(), lst.Front());  // 7 == 7
+    list.PopFront();  // list = {} и lst = {7}
+    ASSERT_NE(list.Front(), lst.Front());  // list.Front() --> UB, так как list пустой
     lst.PopFront();
   }
+
+  */
+
+  // Можно сделать такой тест:
+  while (!lst.IsEmpty()) {
+    ASSERT_EQ(list.Front(), lst.Front());  // Значения соответствующих элементов должны быть равны
+    ASSERT_NE(&list.Front(), &lst.Front());  // Но указатели на соответствующие элементы должны быть разными
+    list.PopFront();
+    lst.PopFront();
+  }
+  ASSERT_TRUE(list.IsEmpty());  // В конце оба списка
+  ASSERT_TRUE(lst.IsEmpty());   // должны быть пустыми
 }
 
+// OK
 TEST_F(ListTest, CopyAssigment) {
-  ForwardList<int> lst;
+  ForwardList<int> lst;  // lst = {4}
   lst.PushFront(4);
-  list = lst;
+  list = lst;  // list = {4}
   ASSERT_NE(&list, &lst) << "Copy assigment must do copy!\n";
   ASSERT_EQ(list.Size(), lst.Size());
   while (!lst.IsEmpty()) {
@@ -117,9 +148,14 @@ TEST_F(ListTest, CopyAssigment) {
     list.PopFront();
     lst.PopFront();
   }
+  ASSERT_TRUE(list.IsEmpty());  // В конце оба списка
+  ASSERT_TRUE(lst.IsEmpty());   // должны быть пустыми
 }
 
+// NOT OK  -----  НИКОГДА НЕ ПРОХОДИТ
 TEST_F(ListTest, SelfAssignment) {
+  /*
+
   std::thread thread([&](){
     list = list;
   });
@@ -128,53 +164,115 @@ TEST_F(ListTest, SelfAssignment) {
     future.wait_for(std::chrono::seconds(1)),
     std::future_status::timeout
   ) << "There is infinity loop!\n";
+
+  */
+
+  // Задача лежит в потоке дольше секунды, но не из-за рекурсии
+  ForwardList<int> copy = list;
+  list = list;
+  while (!list.IsEmpty()) {
+    ASSERT_EQ(list.Front(), copy.Front());
+    list.PopFront();
+    copy.PopFront();
+  }
 }
 
+// NOT OK
 TEST_F(ListTest, RangeWithIteratorPreFix) {
   ASSERT_EQ(std::distance(list.Begin(), list.End()), sz) << 
                 "Distanse between begin and end iterators ins't equal size";
+  /*
+
   int iter = 1;
   for (auto it = list.Begin(); it != list.End(); ++it) {
     ASSERT_EQ(*it, iter++);
   }
+
+  */
+
+  // list = {7, 6, 5, 4, 3, 2, 1}, см. в начало файла
+  int number = 7;
+  for (auto it = list.Begin(); it != list.End(); ++it) {
+    ASSERT_EQ(*it, number--);
+  }
 }
 
+// NOT OK
 TEST_F(ListTest, RangeWithIteratorPostFix) {
   ASSERT_EQ(std::distance(list.Begin(), list.End()), sz) << 
                 "Distanse between begin and end iterators ins't equal size";
+  /*
+
   int iter = 1;
   for (auto it = list.Begin(); it != list.End(); it++) {
     ASSERT_EQ(*it, iter++);
   }
-}
 
-TEST_F(ListTest, EraseBegin) {
-  int second_value = *(list.Begin()++);
-  list.EraseAfter(list.Begin());
-  ASSERT_EQ(list.Size(), sz - 1);
-  ASSERT_NE(*(list.Begin()++), second_value);
-}
-
-TEST_F(ListTest, EraseMedium) {
-  auto it = list.Begin();
-  std::advance(it, list.Size() / 2);
-  list.EraseAfter(it);
-  ASSERT_EQ(list.Size(), sz - 1);
-  for (auto it = list.Begin(); it != list.End(); ++it) {
-    ASSERT_NE(*it, 4);
+  */
+  int number = 7;
+  for (auto it = list.Begin(); it != list.End(); it++) {
+    ASSERT_EQ(*it, number--);
   }
 }
 
+// NOT OK
+TEST_F(ListTest, EraseBegin) {
+  // /*
+
+  // int second_value = *(list.Begin()++);  // list = {7, 6, 5, 4, 3, 2, 1}, *list.Begin()  -->  7, *list.Begin()++  -->  7
+
+  // */
+  // int second_value = *(++list.Begin());  // second_value = 6
+
+  // list.EraseAfter(list.Begin());  // list = {7, 5, 4, 3, 2, 1}
+  // ASSERT_EQ(list.Size(), sz - 1);  // 6 = 6
+  // /*
+  
+  // ASSERT_NE(*(list.Begin()++), second_value);  // *(list.Begin()++)  --> 7, second_value = 6
+  
+  // */
+  // ASSERT_NE(*(++list.Begin()), second_value);
+
+
+  // Я бы тест переделал. Хочется проверить, что другие итераторы не ломаются, а также элементы не меняются порядка
+  auto it2 = ++list.Begin();
+  auto it3 = it2;
+  ++it3;
+  int second_value = *it2;  // second_value = 6
+  int third_value = *it3;  // third_value = 5
+
+  list.EraseAfter(list.Begin());  // it2 теперь сломан, а вот it3 - нет
+  ASSERT_EQ(list.Size(), sz - 1);
+  
+  ASSERT_NE(*(++list.Begin()), second_value);
+  ASSERT_EQ(*(++list.Begin()), third_value);
+  ASSERT_EQ(*it3, third_value);
+}
+
+// NOT OK
+TEST_F(ListTest, EraseMedium) {
+  auto it = list.Begin();  // *it = 7
+  std::advance(it, list.Size() / 2);  // *it = 4
+  list.EraseAfter(it);  // list = {7, 6, 5, 4, 2, 1}
+  ASSERT_EQ(list.Size(), sz - 1);
+  for (auto it = list.Begin(); it != list.End(); ++it) {
+    // ASSERT_NE(*it, 4);  // Был удалён элемент со значением 3
+    ASSERT_NE(*it, 3);
+  }
+}
+
+// OK
 TEST_F(ListTest, InsertMedium) {
-  auto it = list.Begin();
-  std::advance(it, list.Size() / 2);
-  list.InsertAfter(it, 4);
+  auto it = list.Begin();  // *it = 7
+  std::advance(it, list.Size() / 2);  // *it = 4
+  list.InsertAfter(it, 4);  // list = {7, 6, 5, 4, +4, 3, 2, 1}
   ASSERT_EQ(list.Size(), sz + 1);
-  it = list.Begin();
-  std::advance(it, list.Size() / 2);
+  it = list.Begin();  // *it = 7
+  std::advance(it, list.Size() / 2); // *it = 4 (новая четвёрка)
   ASSERT_EQ(*it, 4);
 }
 
+// OK
 TEST_F(ListTest, Clear) {
   list.Clear();
   ASSERT_TRUE(list.IsEmpty());
