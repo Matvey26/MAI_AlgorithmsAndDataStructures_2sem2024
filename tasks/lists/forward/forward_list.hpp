50145b383727aa19a0152efc5013a110861c1813
- - <>
2024-04-05 19:20:28 +0300

    Пупупу

--- tasks/lists/forward/forward_list.hpp
+++ tasks/lists/forward/forward_list.hpp
@@ -1,120 +1,244 @@
 #pragma once
 
-#include <cstdlib>
-#include <iterator>
-#include <functional>
-#include <utility>
-
 #include <fmt/core.h>
 
+#include <cstdlib>
+#include <functional>
+#include <iterator>
+#include <utility>
+
+#include "exceptions.hpp"
+
+// Просто комментарий, чтобы были какие-то изменения. Я просто пытаюсь заставить клиппи самому обновить тесты в моём
+// пр'е.
 
 template <typename T>
-class ForwardList{
+class ForwardList {
 private:
-  class Node{
-    friend class ForwardListIterator;
-    friend class ForwardList;
-    /*???*/
+    struct BaseNode {
+        BaseNode* next_ = nullptr;
+    };
+
+    struct Node : public BaseNode {
+        T data_;
+        explicit Node(const T& value) : data_(value) {
+        }
+    };
+
+public:
+    template <bool IsConst>
+    class BaseIterator {
+        friend class ForwardList;
+
+    public:
+        // NOLINTNEXTLINE
+        using basenode_ptr_type = std::conditional_t<IsConst, const BaseNode*, BaseNode*>;
+        // NOLINTNEXTLINE
+        using node_ptr_type = std::conditional_t<IsConst, const Node*, Node*>;
+        // NOLINTNEXTLINE
+        using value_type = T;
+        // NOLINTNEXTLINE
+        using reference_type = std::conditional_t<IsConst, const T&, T&>;
+        // NOLINTNEXTLINE
+        using pointer_type = std::conditional_t<IsConst, const T*, T*>;
+        // NOLINTNEXTLINE
+        using difference_type = std::ptrdiff_t;
+        // NOLINTNEXTLINE
+        using iterator_category = std::forward_iterator_tag;
+
+        inline bool operator==(const BaseIterator& other) const {
+            return this->current_ == other.current_;
+        }
+
+        inline bool operator!=(const BaseIterator& other) const {
+            return this->current_ != other.current_;
+        }
+
+        inline reference_type operator*() const {
+            return static_cast<node_ptr_type>(this->current_)->data_;
+        }
+
+        BaseIterator& operator++() noexcept {
+            this->current_ = this->current_->next_;
+            return *this;
+        }
+
+        BaseIterator operator++(int) noexcept {
+            BaseIterator temp = *this;
+            this->current_ = this->current_->next_;
+            return temp;
+        }
+
+        inline pointer_type operator->() const {
+            return &static_cast<node_ptr_type>(this->current_)->data_;
+        }
 
     private:
-      /*???*/
+        explicit BaseIterator(basenode_ptr_type node) noexcept : current_(node) {
+        }
 
-  };
-public:
-  class ForwardListIterator{
-    public:
-    /*???*/
-
-  private:
-      ForwardListIterator(const Node*) {
-          // Not implemented
-      }
-  private:
-      Node* current;
-  };
+    private:
+        basenode_ptr_type current_;
+    };
 
 public:
-  ForwardList() {
-    // Not implemented
-  }
+    using Iterator = BaseIterator<false>;
+    using ConstIterator = BaseIterator<true>;
 
-  explicit ForwardList(size_t /*sz*/) {
-    // Not implemented
-  }
+public:
+    ForwardList() : sz_(0) {
+    }
 
-  ForwardList(const std::initializer_list<T>& /*values*/) {
-    // Not implemented
-  }
+    explicit ForwardList(size_t sz) : ForwardList() {
+        while (sz--) {
+            this->PushFront(T());
+        }
+    }
 
-  ForwardList(const ForwardList& /*other*/) {
-    // Not implemented
-  }
+    ForwardList(const std::initializer_list<T>& values) : ForwardList() {
+        this->RangeInitialize(values.begin(), values.end());
+    }
 
-  ForwardList& operator=(const ForwardList& /*other*/) {
-    std::abort(); // Not implemented
-  }
+    ForwardList(const ForwardList& other) : ForwardList() {
+        this->RangeInitialize(other.Begin(), other.End());
+    }
 
-  ForwardListIterator Begin() const noexcept {
-    std::abort(); // Not implemented
-  }
+    template <typename InputIterator>
+    void RangeInitialize(InputIterator first, InputIterator last) {
+        BaseNode* to = &this->head_;
+        for (; first != last; ++first) {
+            to->next_ = static_cast<BaseNode*>(new Node(*first));
+            to = to->next_;
+            ++this->sz_;
+        }
+    }
 
-  ForwardListIterator End() const noexcept {
-    std::abort(); // Not implemented
-  }
+    ForwardList& operator=(const ForwardList& other) {
+        if (this != &other) {
+            ForwardList copy = other;
+            this->Swap(copy);
+        }
+        return *this;
+    }
 
-  inline T& Front() const {
-    std::abort(); // Not implemented
-  }
+    Iterator BeforeBegin() noexcept {
+        return Iterator(&this->head_);
+    }
 
-  inline bool IsEmpty() const noexcept {
-    std::abort(); // Not implemented
-  }
+    ConstIterator BeforeBegin() const noexcept {
+        return ConstIterator(&this->head_);
+    }
 
-  inline size_t Size() const noexcept {
-    std::abort(); // Not implemented
-  }
+    ConstIterator CBeforeBegin() const noexcept {
+        return ConstIterator(&this->head_);
+    }
 
-  void Swap(ForwardList& /*a*/) {
-    // Not implemented
-  }
+    Iterator Begin() noexcept {
+        return Iterator(this->head_.next_);
+    }
 
-  void EraseAfter(ForwardListIterator /*pos*/) {
-    // Not implemented
-  }
+    ConstIterator Begin() const noexcept {
+        return ConstIterator(this->head_.next_);
+    }
 
-  void InsertAfter(ForwardListIterator /*pos*/, const T& /*value*/) {
-    // Not implemented
-  }
+    Iterator End() noexcept {
+        return Iterator(nullptr);
+    }
 
-  ForwardListIterator Find(const T& /*value*/) const {
-    std::abort(); // Not implemented
-  }
+    ConstIterator End() const noexcept {
+        return ConstIterator(nullptr);
+    }
 
-  void Clear() noexcept {
-    // Not implemented
-  }
+    inline T& Front() const {
+        return static_cast<Node*>(this->head_.next_)->data_;
+    }
 
-  void PushFront(const T& /*value*/) {
-    // Not implemented
-  }
+    inline bool IsEmpty() const noexcept {
+        return this->sz_ == 0;
+    }
 
-  void PopFront() {
-    // Not implemented
-  }
+    inline size_t Size() const noexcept {
+        return this->sz_;
+    }
 
-  ~ForwardList() {
-    // Not implemented
-  }
+    void Swap(ForwardList& other) {
+        std::swap(this->head_.next_, other.head_.next_);
+        std::swap(this->sz_, other.sz_);
+    }
+
+    void EraseAfterNode(BaseNode* current) {
+        Node* node_to_del = static_cast<Node*>(current->next_);
+        current->next_ = node_to_del->next_;
+        delete node_to_del;
+        --this->sz_;
+    }
+
+    void InsertAfterNode(BaseNode* current, const T& value) {
+        Node* node_to_add = new Node(value);
+        node_to_add->next_ = current->next_;
+        current->next_ = static_cast<BaseNode*>(node_to_add);
+        ++this->sz_;
+    }
+
+    void EraseAfter(ConstIterator pos) {
+        // NOLINTNEXTLINE
+        EraseAfterNode(const_cast<BaseNode*>(pos.current_));
+    }
+
+    void EraseAfter(Iterator pos) {
+        EraseAfterNode(pos.current_);
+    }
+
+    void InsertAfter(ConstIterator pos, const T& value) {
+        // NOLINTNEXTLINE
+        InsertAfterNode(const_cast<BaseNode*>(pos.current_), value);
+    }
+
+    void InsertAfter(Iterator pos, const T& value) {
+        InsertAfterNode(pos.current_, value);
+    }
+
+    Iterator Find(const T& value) {
+        auto it = this->Begin();
+        for (; it != this->End(); ++it) {
+            if (*it == value) {
+                break;
+            }
+        }
+        return it;
+    }
+
+    void Clear() noexcept {
+        while (this->sz_) {
+            this->PopFront();
+        }
+    }
+
+    void PushFront(const T& value) {
+        this->InsertAfter(CBeforeBegin(), value);
+    }
+
+    void PopFront() {
+        if (this->sz_ == 0) {
+            throw ListIsEmptyException("Can't delete element from empty forward list!");
+        }
+        this->EraseAfter(CBeforeBegin());
+    }
+
+    ~ForwardList() {
+        this->Clear();
+    }
 
 private:
-  /*???*/
+    size_t sz_;
+    BaseNode head_;
 };
 
-
 namespace std {
-  // Global swap overloading
-  template <typename T>
-  void swap(ForwardList<T>& a, ForwardList<T>& b) {
+// Global swap overloading
+template <typename T>
+// NOLINTNEXTLINE
+void swap(ForwardList<T>& a, ForwardList<T>& b) {
     a.Swap(b);
-  }
 }
+}  // namespace std
